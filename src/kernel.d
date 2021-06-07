1f9e3bda72d2b14b9f580e69e33b7e1503f3604f
codic <codic@linuxmail.org>
2021-06-06 17:50:28 -0700

    cleanup

--- src/kernel.d
+++ src/kernel.d
@@ -5,15 +5,15 @@ import src.ports;
 
 extern (C) void main(uint magic, uint addr)
 {
-	int ypos = 0; //Starting points of the cursor
+	int ypos = 0;
 	int xpos = 0;
-	const uint COLUMNS = 80; //Screensize
+	const uint COLUMNS = 80;
 	const uint LINES = 25;
 
-	ubyte* vidmem = cast(ubyte*) 0xFFFF_8000_000B_8000; //Video memory address
-
+	ubyte* vidmem = cast(ubyte*) 0xb8000;
+	
 	for (int i = 0; i < COLUMNS * LINES * 2; i++)
-	{ //Loops through the screen and clears it
+	{
 		volatileStore(vidmem + i, 0);
 	}
 
